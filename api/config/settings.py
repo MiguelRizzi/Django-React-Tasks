17409d69a5bddb15ef91e8b68058aa702b0b1fbc
Miguel Angel Rizzi <miguel-rizzi@hotmail.com.ar>
2023-07-25 13:51:21 -0300

    Se crea app tasks

--- api/config/settings.py
+++ api/config/settings.py
@@ -15,6 +15,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#  sys.path para agregar la ruta de la carpeta "apps" para que se pueda importar las apps correctamente.
+import sys
+import os
+
+APPS_DIR = os.path.join(BASE_DIR, 'apps')
+sys.path.append(str(APPS_DIR))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -37,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # mis apps
+    'tasks.apps.TasksConfig',
 ]
 
 MIDDLEWARE = [

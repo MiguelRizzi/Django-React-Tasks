80525853e6ada181c5656e8a3e0aee065b646b97
Miguel Angel Rizzi <miguel-rizzi@hotmail.com.ar>
2023-07-25 13:58:11 -0300

    Se crea model tasks

--- api/apps/tasks/admin.py
+++ api/apps/tasks/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Tasks
 # Register your models here.
+@admin.register(Tasks)
+class TasksAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "description",
+        "creation_date",
+        "completed",
+    )
+    search_fields = ("title", "completed", "creation_date",)
+    ordering = ("creation_date",)
+    list_filter = ("completed", "creation_date",)
+    
